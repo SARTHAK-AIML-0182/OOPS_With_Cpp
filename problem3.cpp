119548cd6400074c3794df1eefa163b825b83188
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-09-16 09:57:34 +0530

    OOPS Program done in Class

--- problem3.cpp
+++ problem3.cpp
@@ -1,24 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 class Patient {
-   public:
+public:
     string name;
     int id;
     static int activePatients;
-    Patient(string n,int ID) {
-        
+    Patient(string n, int ID) {
         name = n;
         id = ID;
         activePatients++;
     }
-    static void displayActivePatients() {
-        cout << "Active Patients: " << activePatients << endl;
-    }
     Patient() {
         activePatients++;
     }
+    ~Patient() {
+        activePatients--;
+    }
+    static void displayActivePatients() {
+        cout << "Active Patients: " << activePatients << endl;
+    }
 };
+
 int Patient::activePatients = 0;
+
 int main() {
     Patient p1("Sarthak", 101);
     Patient p2("Shivam", 102);
